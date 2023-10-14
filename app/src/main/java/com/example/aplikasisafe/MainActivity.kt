c150a4e75c0a09ee4f76feb3e425b1fb57d72c27
Lazkerz <Laziranurul@gmail.com>
2023-10-14 14:16:52 +0700

    first

---
+++ app/src/main/java/com/example/aplikasisafe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aplikasisafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Buge?) {
+        super.gionCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
