670a1f9837b98a1fa9caff20a88f4d0133c1be34
Muhammad Fakhri Musyaffa <mfakhrim643@gmail.com>
2023-10-16 20:58:06 +0700

    revisi-2

--- app/src/main/java/com/example/aplikasisafe/pelatihan/DaftarKerjaActivity.kt
+++ app/src/main/java/com/example/aplikasisafe/pelatihan/DaftarKerjaActivity.kt
@@ -1,12 +1,28 @@
 package com.example.aplikasisafe.pelatihan
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.aplikasisafe.R
 
-class DaftarKerjaActivity : AppCompatActivity() {
+class DaftarKerjaActivity : AppCompatActivity(), View.OnClickListener {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daftar_kerja)
+        val btnRegisterKerja: Button = findViewById(/* id = */ R.id.btn_registerKerja)
+        btnRegisterKerja.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_registerKerja -> {
+                val intent = Intent(this@DaftarKerjaActivity, PendaftaranBerhasilActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
