0683dc5bdb925949a26087aae4be1f8cfd81f5f2
Muhammad Fakhri Musyaffa <mfakhrim643@gmail.com>
2023-10-18 10:07:42 +0700

    revisiiy

--- app/src/main/java/com/example/aplikasisafe/MainActivity.kt
+++ app/src/main/java/com/example/aplikasisafe/MainActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    BottomnNavigationView buttomNavigationView;
-
-    HomeFragement
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
